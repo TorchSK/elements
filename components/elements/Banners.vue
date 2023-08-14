7e41a6e3bb4732a3beabbfe834701793853cfc10
TorchSK <gtorch@gmail.com>
2023-08-14 07:10:37 +0200

    gg

--- components/elements/Banners.vue
+++ components/elements/Banners.vue
@@ -1,12 +1,12 @@
 <template>
 
     <ul class="grid grid-cols-4">   
-        <li v-for="item in element.data.images.value" class="border aspect-[2/3]">
-
-            <img :src="item.blob" />
+        <li v-for="item in element.data.images.value" class="border relative aspect-[2/3]">
+            <img :src="item" />
+            <div class="absolute left-10 bottom-10 bg-black text-gray-100 px-4 py-2 rounded-md">View</div>
         </li>
-
     </ul>
+
 </template>
 
 <script setup>
