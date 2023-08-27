2ebcb31eb3f37dcaa79bb7a328deaafd927dbcc0
TorchSK <gtorch@gmail.com>
2023-08-27 19:44:08 +0200

    ff

--- components/elements/Banners.vue
+++ components/elements/Banners.vue
@@ -1,13 +1,13 @@
 <template>
 
-    <ul class="grid grid-cols-4">   
-        <li v-for="item in element.data.banners" class="border relative aspect-[2/3]">
-            <img :src="item.image" />
-            <div class="absolute text-left left-8 bottom-25">
-                <div class="mr-8 bg-gray-200/80 text-gray-800 px-3.5 py-1.5 text-2xl font-bold rounded-md mb-2">{{ item.heading }}</div>
-                <div class="mr-8 bg-gray-200/80 text-gray-800 px-3.5 py-1.5 text-lg rounded-md">{{ item.text }}</div>
+    <ul class="grid grid-cols-2 lg:grid-cols-4 gap-3 px-3 lg:px-0">   
+        <li v-for="item in element.data.banners" class="relative aspect-[2/3] rounded-xl overflow-hidden">
+            <nuxt-img :src="item.image" class="max-w-full max-h-full"></nuxt-img>
+            <div class="absolute text-left left-4 lg:left-8 bottom-25">
+                <div class="mr-4 lg:mr-8 bg-gray-200/80 text-gray-800 px-3.5 py-1.5 text-base lg:text-2xl font-bold rounded-md mb-2">{{ item.heading }}</div>
+                <div class="mr-4 lg:mr-8 bg-gray-200/80 text-gray-800 px-3.5 py-1.5 text-sm lg:text-lg rounded-md">{{ item.text }}</div>
             </div>
-            <div class="absolute left-10 bottom-10 bg-gray-900/80 text-gray-100 px-3.5 py-1.5 text-sm rounded-md">View</div>
+            <div class="absolute left-10 bottom-10 bg-gray-900/90 text-gray-100 px-4 py-2 text-sm rounded-md font-semibold">View</div>
         </li>
     </ul>
 
---
+++ components/elements/Products.vue
@@ -0,0 +1,27 @@
+<template>
+
+<div>
+
+    <div v-if="element.data.caption" class="mb-3 font-extrabold text-2xl text-center mb-6 md:text-left text-gray-800">{{element.data.caption}}</div>
+
+    <div v-if="element.data.products.length" class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 gap-6 flex-grow">
+        <ProductsBox v-for="product in element.data.products" :product="product"/>
+    </div>
+
+	<div v-else class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 gap-6 flex-grow">
+        <ProductsSkeleton v-for="product in [1,2,3,4,5,6]" :product="product"/>
+    </div>
+	
+</div>
+
+</template>
+
+<script setup>
+
+	const props = defineProps({
+		element: {
+			type: Object
+		}
+	})
+
+</script>
