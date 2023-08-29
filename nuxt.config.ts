7856ad5ca5fd83c70c96306ea88aceef925f7aa6
TorchSK <gtorch@gmail.com>
2023-08-29 12:47:29 +0200

    ss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   alias: {
 		"vue": require.resolve("vue"),
+    "vue/server-renderer": require.resolve("vue/server-renderer"),
 	},
 })
