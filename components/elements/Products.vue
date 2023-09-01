0d7d4ae3b7d8bc0b10bfdaf7cfe3e41426681a3d
TorchSK <gtorch@gmail.com>
2023-09-01 20:41:20 +0200

    dd

--- components/elements/Products.vue
+++ components/elements/Products.vue
@@ -1,9 +1,8 @@
 <template>
 
 <div>
-
     <div v-if="element.data.caption" class="mb-3 font-extrabold text-2xl text-center mb-6 md:text-left text-gray-800">{{element.data.caption}}</div>
-
+    
     <div v-if="element.data.products.length" class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 gap-6 flex-grow">
         <ProductsBox v-for="product in element.data.products" :product="product"/>
     </div>
