b01ce83e3aa9aace6b704a5da09c405acd19d566
TorchSK <gtorch@gmail.com>
2023-09-17 19:37:27 +0200

    dd

--- components/elements/Panel.vue
+++ components/elements/Panel.vue
@@ -1,7 +1,11 @@
 <template>
-	<div :class="[element.data.bgColor.value, element.data.textColor.value]" class="rounded-sm flex group items-center relative">
-		<div class="flex-grow flex-shrink-0 text-center p-4">{{element.data.text.value}}</div>
-        <nuxt-link :to="element.data.link.value" v-if="element.data.link.value" class="w-14 flex justify-center">
+	<div :class="{
+    	'bg-green-500 text-white':element.data.color == 'primary',
+    	}" 
+		class="rounded-md flex group items-center relative"
+	>
+		<div class="flex-grow flex-shrink-0 text-center p-4 font-medium">{{element.data.text}}</div>
+        <nuxt-link :to="element.data.link" v-if="element.data.link" class="w-14 flex justify-center">
             <div class="i-ph-arrow-circle-right text-2xl group-hover:text-3xl transition-all" />
         </nuxt-link>
 	</div>
