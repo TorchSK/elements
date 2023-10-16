bac5df6f3491866896d1eaa18406d48a2d6bfe89
TorchSK <gtorch@gmail.com>
2023-10-16 07:43:11 +0200

    ff

--- components/elements/Banners.vue
+++ components/elements/Banners.vue
@@ -3,11 +3,11 @@
     <ul class="grid grid-cols-2 lg:grid-cols-4 gap-3 px-3 lg:px-0">   
         <li v-for="item in element.data.banners" class="relative aspect-[2/3] rounded-xl overflow-hidden bg-gray-100">
             <Image :image="item.image" class="max-w-full max-h-full" />
-            <div class="absolute text-left left-4 lg:left-8 bottom-25">
+            <div class="absolute text-left left-4 lg:left-8" :class="[item.position == 'top' ? 'top-15' : 'bottom-25']">
                 <div class="mr-4 lg:mr-8 text-gray-800 px-3.5 py-1.5 text-base lg:text-2xl font-bold rounded-md mb-2">{{ item.heading }}</div>
                 <div class="mr-4 lg:mr-8 text-gray-800 px-3.5 py-1.5 text-sm lg:text-lg rounded-md">{{ item.text }}</div>
             </div>
-            <div class="absolute left-10 bottom-10 bg-gray-900/90 text-gray-100 px-4 py-2 text-sm rounded-md font-semibold">View</div>
+            <nuxt-link :to="item.link" class="absolute left-10 bottom-10 bg-gray-900/90 text-gray-100 px-4 py-2 text-sm rounded-md font-semibold">View</nuxt-link>
         </li>
     </ul>
 
--- components/elements/Panel.vue
+++ components/elements/Panel.vue
@@ -1,10 +1,8 @@
 <template>
-	<div :class="['bg-'+element.data.bgColor]" 
-		class="rounded-md flex group items-center relative"
-	>
-		<div class="flex-grow flex-shrink-0 text-center p-4 font-medium" :class="['text-'+element.data.textColor]">{{element.data.text}}</div>
-        <nuxt-link :to="element.data.link" v-if="element.data.link" class="w-14 flex justify-center">
-            <div class="i-ph-arrow-circle-right text-2xl group-hover:text-3xl transition-all" />
+	<div :class="['bg-'+element.data.bgColor]" class="rounded-md flex group items-center relative">
+		<div class="flex-grow flex-shrink-0 text-center font-medium" :class="['text-'+element.data.textColor, padding]">{{element.data.text}}</div>
+        <nuxt-link :to="element.data.link" v-if="element.data.link" class="w-14 flex justify-center absolute right-1">
+            <div class="i-ph-arrow-circle-right text-2xl group-hover:text-3xl transition-all" :class="['text-'+element.data.textColor]" />
         </nuxt-link>
 	</div>
 </template>
@@ -16,6 +14,16 @@
 			type: Object
 		}
 	})
+
+	const padding = computed(() => {
+		if(!props.element.data.size || props.element.data.size == 'base') return 'p-4'
+		else if(props.element.data.size == 'xs') return 'p-2 text-sm'
+		else if(props.element.data.size == 'sm') return 'p-3'
+		else if(props.element.data.size == 'lg') return 'p-6'
+		else if(props.element.data.size == 'xl') return 'p-8 text-xl'
+	})
+
+
 
 
 </script>
