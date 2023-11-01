4cf1b01dffc7e3008da03d3dd6175ded4ce397cb
TorchSK <gtorch@gmail.com>
2023-11-01 07:40:39 +0100

    oo

--- components/elements/Banner.vue
+++ components/elements/Banner.vue
@@ -1,16 +1,16 @@
 <template>
-    <div>
-    <template v-if="element.data.link.value">
-	    <nuxt-link :to="element.data.link.value">
-            <nuxt-img :src="src" class="w-full" width="1280" height="465" placeholder sizes="sm:100vw md:50vw lg:400px" />
-        </nuxt-link>
-    </template>
-
-    <template v-else>
-	    <nuxt-img :src="src" class="w-full" width="1280" height="465" placeholder sizes="sm:100vw md:50vw lg:400px" />
-    </template>
+    
+    <div class="relative">
+        <Image :image="element.data.image" class="h-full w-full object-cover object-center" />
+        <div class="absolute inset-0 flex flex-col items-center justify-center z-100">
+            <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Long-term thinking</h2>
+            <p class="mt-3 text-xl text-white">{{ element.data.text }}</p>
+        </div>
+        <div aria-hidden="true" class="absolute inset-0 bg-black bg-opacity-40 z-99" />
     </div>
+
 </template>
+
 
 <script setup>
 
@@ -19,7 +19,5 @@
 			type: Object
 		}
 	})
-
-    const { src } = useCdnImage(props.element.data.banner.value, 'banner')
 
 </script>
